1b24bbdb6d6227454fa09f6fa9f5bd818d4725ab
Peter Schrammel <peter.schrammel@cs.ox.ac.uk>
2016-05-30 12:08:21 +0100

    fully-unwound check fixed as far as possible

--- src/domains/incremental_solver.h
+++ src/domains/incremental_solver.h
@@ -19,11 +19,10 @@ Author: Peter Schrammel
 #include "domain.h"
 #include "util.h"
 
-//#define DISPLAY_FORMULA
 //#define NO_ARITH_REFINEMENT
 //#define NON_INCREMENTAL // (experimental)
 
-#define DISPLAY_FORMULA
+//#define DISPLAY_FORMULA
 //#define DEBUG_FORMULA
 //#define DEBUG_OUTPUT
 
@@ -93,8 +92,21 @@ class incremental_solvert : public messaget
     solver->set_assumptions(whole_formula);
 #endif
 #endif
-
+#if defined(DEBUG_FORMULA) && defined(DEBUG_OUTPUT)
+    decision_proceduret::resultt result = (*solver)();
+    if(result==decision_proceduret::D_UNSATISFIABLE)
+    {
+      for(unsigned i=0; i<formula_expr.size(); i++) 
+      {
+        if(solver->is_in_conflict(formula[i]))
+          std::cout << "is_in_conflict: " 
+              << from_expr(ns, "", formula_expr[i]) << std::endl;
+      }
+    }
+    return result;
+#else
     return (*solver)();    
+#endif
   }
 
   exprt get(const exprt& expr) { return solver->get(expr); }
@@ -123,6 +135,7 @@ class incremental_solvert : public messaget
 
   //for debugging
   bvt formula;
+  exprt::operandst formula_expr;
   void debug_add_to_formula(const exprt &expr);
 
   //context assumption literals
